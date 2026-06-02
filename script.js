c008e0109ec0078698487dc53f61f5a81e570643
SrishtiDangi <dangisrishti0@gmail.com>
2026-06-02 16:53:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleSidebar(){
+
+    let sidebar =
+    document.getElementById("sidebar");
+
+    if(sidebar.style.left === "0px"){
+
+        sidebar.style.left = "-250px";
+    }
+
+    else{
+
+        sidebar.style.left = "0px";
+    }
+}
